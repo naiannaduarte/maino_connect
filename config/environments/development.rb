dbb934c98220cee5051464acd07cb864e826bd1a
Naianna <naianna.sdc@gmail.com>
2025-01-01 13:57:49 +0000

    ajustes de foormatação

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,7 +46,7 @@ Rails.application.configure do
     port: 587,
     domain: "gmail.com",
     authentication: "plain",
-    #enable_starttls_auto: true,
+    # enable_starttls_auto: true,
     user_name: ENV["EMAIL_USERNAME"],
     password: ENV["EMAIL_PASSWORD"]
   }
