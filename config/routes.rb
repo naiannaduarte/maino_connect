4902be7d039df4d5181114b16f7fed104236e91b
Naianna <naianna.sdc@gmail.com>
2025-01-03 15:17:01 +0000

    Ajustando fluxo de telas para Perfil de usuário

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "home/index"
   root "home#index"
   get "up" => "rails/health#show", as: :rails_health_check
-  resources :posts, only: [ :index, :new, :create ] do
+  resources :posts, only: [ :index, :new, :create, :show ] do
     resources :comments, only: [ :create ]
   end
 end
